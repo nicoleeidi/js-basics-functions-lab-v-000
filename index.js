b5b6a58c5fba1db383eedc5125df45b0a951c24b
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-16 15:03:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,25 +4,30 @@ function distanceFromHqInBlocks(location){
   return distance
 }
 function distanceFromHqInFeet(location){
-  distanceFromHqInBlocks() * 264 
+  distanceFromHqInBlocks() * 264
 }
 function distanceTravelledInFeet(startblock,endblock){
   distance= (endblock- startblock)*264
-  return distance
+  if (endblock > startblock ){
+    return distance
+  }
+  else if (startblock > endblock) {
+    return distance * -1
+  }
 }
 function calculatesFarePrice(startblock,endblock){
   distance= endblock- startblock
   if (distance <= 400) {
     return 0
-  } 
+  }
   else if (distance > 400 && distance <= 2000) {
-      return distance * 2 
+      return distance * 2
   }
   else if (distance > 2000 && distance <= 2500){
-    return 2 * 2500 
+    return 2 * 2500
   }
   else if (distance > 2500) {
     return 'cannot travel that far'
   }
-    
+
 }
