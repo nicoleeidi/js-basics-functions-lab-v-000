fb28a714fe93b3271f8f3b9646b8b2df94cc961a
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-16 14:51:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+  distance= 42 - location 
+  return distance 
+}
+function distanceFromHqInFeet(location){
+  distanceFromHqInBlocks() 
+}
+distanceTravelledInFeet(startblock,endblock){
+  distance= (endblock- startblock)*264 
+  return distance 
+}
