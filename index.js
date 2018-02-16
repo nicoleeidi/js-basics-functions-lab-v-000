b9a732d38db8e4d0f182588121b8cd4831b13ff4
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-16 14:59:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ function distanceTravelledInFeet(startblock,endblock){
   distance= (endblock- startblock)*264
   return distance
 }
+function calculatesFarePrice(startblock,endblock){
+  distance= endblock- startblock
+  if (distance <= 400) {
+    return 0
+  } 
+  else if (distance > 400 && distance <= 2000) {
+      return distance * 2 
+  }
+  else if (distance > 2000 && distance <= 2500){
+    return 2 * 2500 
+  }
+  else if (distance > 2500) {
+    return 'cannot travel that far'
+  }
+    
+}
